6b246cf4fe0c8f8a169d053fbb1791298e9a4a1f
AoifMurphy <79363725+AoifMurphy@users.noreply.github.com>
2021-04-09 23:45:23 +0100

    April 9th - Starbucks data added to test

---
+++ Testng.py
@@ -0,0 +1,12 @@
+import pandas as pd
+customer_profile = pd.read_csv(r'profile.csv')
+print(customer_profile.head())
+
+customer_portfolio = pd.read_csv(r'portfolio.csv')
+print(customer_portfolio)
+
+customer_transcript = pd.read.csv(r'transcript.csv')
+print(head())
+
+# This is customer data from Starbucks.
+
