a9ef9520dab3829e7a33ee5782301c9834a36db3
AoifMurphy <79363725+AoifMurphy@users.noreply.github.com>
2021-04-10 14:19:45 +0100

    April 10th - Telco data added to test

--- Testng.py
+++ Testng.py
@@ -1,12 +1,25 @@
 import pandas as pd
+import numpy as np
 customer_profile = pd.read_csv(r'profile.csv')
 print(customer_profile.head())
 
 customer_portfolio = pd.read_csv(r'portfolio.csv')
 print(customer_portfolio)
 
-customer_transcript = pd.read.csv(r'transcript.csv')
-print(head())
+# Lists
 
-# This is customer data from Starbucks.
+x = [2, 5, 4, 0, 7, 1]
+
+print(x[0])
+
+np_2d = np.array([[2,3],[4,5]])
+print(np_2d[-2,1])
+
+p = 3
+q = "Hello! "
+print(q * p)
+
+Customer_Churn = pd.read_csv(r'C:\Users\aoife\Desktop\UCDPA_AoifePMurphy\Telco-Customer-Churn.csv')
+print(Customer_Churn.head())
+print(Customer_Churn.columns)
 
