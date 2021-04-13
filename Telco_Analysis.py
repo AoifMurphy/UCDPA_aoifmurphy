817e1679e5a607fdb3717f2bd53706e608598a87
AoifMurphy <79363725+AoifMurphy@users.noreply.github.com>
2021-04-13 21:31:19 +0100

    April 13th - Added functions

--- Telco_Analysis.py
+++ Telco_Analysis.py
@@ -1,5 +1,27 @@
 # import packages
+import pandas as pd
 
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+
+telco_data=pd.read_csv("Telco-Customer-Churn.csv")
+print(telco_data)
+
+print(telco_data.info())
+
+print (telco_data.head())
+
+missing_value=telco_data.isnull().sum
+print(missing_value)
+
+print(telco_data.describe())
+
+def importdata(filename):
+    data=pd.read_csv(filename)
+    print(data.head())
+    print(data.index)
+    print(data.info)
+
+importdata("Telco-Customer-Churn.csv")
+importdata("portfolio.csv")
+importdata("profile.csv")
+importdata("transcript.csv")
