f0a6d025870e2fd95ee7f9dc71cdd1f2c966ae47
AoifMurphy <79363725+AoifMurphy@users.noreply.github.com>
2021-04-16 23:38:08 +0100

    April 16th - Updated code

--- Starbucks_Analysis.py
+++ Starbucks_Analysis.py
@@ -2,13 +2,18 @@ import pandas as pd
 import numpy as np
 
 def import_csv(filename):
-    data = pd.read_csv (filename)
+    data = pd.read_csv(filename)
     print(data)
-    print(data.index())
+    print(data.describe())
+    print(data.columns)
 
-import_csv("Starbucks satisfactory survey.csv")
+CustProf=import_csv("profile.csv")
 
+Cust_Port=import_csv("portfolio.csv")
 
+Cust_Trpt=import_csv("transcript.csv")
+
+StarbucksCSAT=import_csv("Starbucks satisfactory survey.csv")
 
 
 
